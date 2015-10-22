bbcada32127bc124d141ac3566de09f09c3020d9
JD Bothma <jbothma@gmail.com>
2015-10-22 16:44:30 +0200

    add submitted tasks to db

--- app/views.py
+++ app/views.py
@@ -1,15 +1,20 @@
 from flask import render_template, flash, redirect
-from app import app, models
+from app import app, db, models
 from .forms import TaskForm
 
 @app.route('/')
 @app.route('/tasks', methods=['GET', 'POST'])
-def index():
+def task_collection():
     form = TaskForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.title.data, str(form.description.data)))
+        add_task(form.title.data,
+                 form.description.data)
     tasks = models.Task.query.all()
     return render_template('index.html',
                            tasks = tasks,
                            form = form)
+
+def add_task(title, description):
+    task = models.Task(title=title, description=description)
+    db.session.add(task)
+    db.session.commit()
