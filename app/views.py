dd6dbd8e09ff975bbe1546cf978e195b9df26f32
JD Bothma <jbothma@gmail.com>
2015-10-22 16:30:03 +0200

    Replace mock tasks with DB tasks

--- app/views.py
+++ app/views.py
@@ -1,21 +1,6 @@
 from flask import render_template, flash, redirect
-from app import app
+from app import app, models
 from .forms import TaskForm
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': True
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
 
 @app.route('/')
 @app.route('/tasks', methods=['GET', 'POST'])
@@ -24,6 +9,7 @@ def index():
     if form.validate_on_submit():
         flash('Login requested for OpenID="%s", remember_me=%s' %
               (form.title.data, str(form.description.data)))
+    tasks = models.Task.query.all()
     return render_template('index.html',
                            tasks = tasks,
                            form = form)
