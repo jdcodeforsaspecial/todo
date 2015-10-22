69288815f8e7f08a97a28e22fd841ae8b0765231
JD Bothma <jbothma@gmail.com>
2015-10-22 10:23:15 +0200

    add template and 'api'

--- app/views.py
+++ app/views.py
@@ -1,6 +1,28 @@
+from flask import render_template, jsonify
 from app import app
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
